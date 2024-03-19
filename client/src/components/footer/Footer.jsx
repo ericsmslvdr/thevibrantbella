7c7a06f9656f290c5007d02fdda08b269f5f2cbc
Eric <itintern.primecorp@gmail.com>
2024-03-19 18:18:30 +0800

    Add initial design

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='bg-lightPink'>
+            <div className='container mx-auto'>Footer</div>
+        </div>
     )
 }
 
