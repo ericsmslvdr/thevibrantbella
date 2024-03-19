b99af4e90d1464d6d5e5bb0b7e7c40127d21d287
Eric <itintern.primecorp@gmail.com>
2024-03-19 18:05:02 +0800

    Separate folder

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,20 @@
+/* REACT ICONS */
+import { GoSearch as SearchIcon } from "react-icons/go";
+import { IoPersonOutline as PersonOutlineIcon } from "react-icons/io5";
+import { FiShoppingBag as CartIcon } from "react-icons/fi";
+import { TiSocialFacebookCircular as FacebookIcon } from "react-icons/ti";
+import { TbBrandShopee as ShopeeIcon } from "react-icons/tb";
+import { PiTiktokLogo as TiktokIcon } from "react-icons/pi";
+import { IoIosMenu as MenuIcon } from "react-icons/io";
+import { IoMdClose as CloseIcon } from "react-icons/io";
+
+export {
+    SearchIcon,
+    PersonOutlineIcon,
+    CartIcon,
+    FacebookIcon,
+    ShopeeIcon,
+    TiktokIcon,
+    MenuIcon,
+    CloseIcon
+};
--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,23 +1,2 @@
 export { default as thevibrantbellaIcon } from "./thevibrantbella-icon.jpg"
 export { default as logoText } from "./logo-text.svg"
-
-/* REACT ICONS */
-import { GoSearch as SearchIcon } from "react-icons/go";
-import { IoPersonOutline as PersonOutlineIcon } from "react-icons/io5";
-import { FiShoppingBag as CartIcon } from "react-icons/fi";
-import { TiSocialFacebookCircular as FacebookIcon } from "react-icons/ti";
-import { TbBrandShopee as ShopeeIcon } from "react-icons/tb";
-import { PiTiktokLogo as TiktokIcon } from "react-icons/pi";
-import { IoIosMenu as MenuIcon } from "react-icons/io";
-import { IoMdClose as CloseIcon } from "react-icons/io";
-
-export {
-    SearchIcon,
-    PersonOutlineIcon,
-    CartIcon,
-    FacebookIcon,
-    ShopeeIcon,
-    TiktokIcon,
-    MenuIcon,
-    CloseIcon
-};
