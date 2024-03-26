2107cdd85b9b2d504e975b0edfb7dc8a6dcc1f70
ericsmslvdr <ericsmsalvador@gmail.com>
2024-03-26 21:20:09 +0800

    Skeleton for new arrival section

--- client/src/pages/Home/components/new-arrivals/NewArrival.jsx
+++ client/src/pages/Home/components/new-arrivals/NewArrival.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export const NewArrival = () => {
     return (
-        <div>
-            <div className=''>New Arrival</div>
+        <div className='container mx-auto w-dvw border-solid border-red-500 border py-8'>
+            <div className='mb-4'>New Arrival</div>
+            <div className='flex gap-2'>
+                <button className='bg-orange-100'>Prev</button>
+                <div className='border border-solid border-green-500 flex gap-4 w-full'>
+                    <div className='border border-solid border-blue-500 h-96 grow'>Product 1</div>
+                    <div className='border border-solid border-blue-500 h-96 grow'>Product 2</div>
+                    <div className='border border-solid border-blue-500 h-96 grow'>Product 3</div>
+                    <div className='border border-solid border-blue-500 h-96 grow'>Product 4</div>
+                </div>
+                <button className='bg-orange-100'>Next</button>
+            </div>
         </div>
     )
 }
