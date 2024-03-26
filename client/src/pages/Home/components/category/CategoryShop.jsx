20d669916c39e42f355dec733637a8c4bc302bde
ericsmslvdr <ericsmsalvador@gmail.com>
2024-03-26 18:42:33 +0800

    Update padding

--- client/src/pages/Home/components/category/CategoryShop.jsx
+++ client/src/pages/Home/components/category/CategoryShop.jsx
@@ -3,7 +3,7 @@ import { CategoryCard } from './CategoryCard'
 
 export const CategoryShop = () => {
     return (
-        <div className='container mx-auto w-dvw border-solid border-orange-500 border p-4'>
+        <div className='container mx-auto w-dvw border-solid border-orange-500 border py-8'>
             <div className='flex flex-wrap gap-4'>
                 <CategoryCard className={'basis-1/4'}>Shop Dress</CategoryCard>
                 <CategoryCard className={'basis-1/4'}>Shop Tops</CategoryCard>
